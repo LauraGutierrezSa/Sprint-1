7849698e4ca42f6b18debee08c82b9c6708a4822
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-01-23 22:21:23 +0100

    Update Sprint1 entrega 1.3.js

--- Sprint1 entrega 1.3.js
+++ Sprint1 entrega 1.3.js
@@ -35,3 +35,40 @@ const arrowFunction = (parameter, callback = (a, b) => {
    };
 
 arrowFunction('sum');
+
+////Nivell 2 - Exercici 1: Donats els objectes employees i salaries, crea una arrow function getEmployee() que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+const getEmployee = () => {
+    return new Promise((resolve, reject) => {
+      if (employees[1]) {
+          resolve(employees[1])
+      } else {
+          reject(`Employee with id ${1} not found`)
+      }
+    });
+  };
+ 
+getEmployee(1).then(employee => console.log(employee));
+getEmployee(5).catch(error => console.log(error))
