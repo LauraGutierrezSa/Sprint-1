b34ffd5c5839f27d70690b9895fcba0cd301ce81
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-01-25 22:12:23 +0100

    nivell 1 - exercicis 1, 2 i 3

--- Sprint 1.5 nivell 1 exercici 1/index.js
+++ Sprint 1.5 nivell 1 exercici 1/index.js
@@ -4,14 +4,40 @@ function writeInFile() {
 const fs = require('fs'); 
 
 fs.writeFile('archivo.txt', 'Hola mundo!!', (err) => { if (err) throw err; 
-    
-//console.log('Archivo guardado!'); 
 });}
 
 writeInFile()
 
 // Exercici 2: Crea una altra funció que mostri per consola el contingut del fitxer anterior.
 
+function readFromFile() {
+    const fs = require('fs'); 
 
+    fs.readFile('archivo.txt', 'utf8', (err, data) => {
+        if (err) throw err; 
+        
+        console.log(data);
+    });
+}
+
+readFromFile();
 
 // Exercici 3: Crea una funció que comprimeixi el fitxer del nivell 1.
+
+const zlib = require('zlib');
+function compressFile() {
+    const fs = require('fs'); 
+    fs.readFile('archivo.txt', (err, data) => {
+        if (err) throw err; 
+        zlib.gzip(data, (err, compressed) => {
+            if (!err) {
+                fs.writeFile('archivo.txt.gz', compressed, (err) => {
+                    if (err) throw err;
+                    console.log("File compressed successfully");
+                });
+            }
+        });
+    });
+}
+
+compressFile();
