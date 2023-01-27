9c30298cf1c3d7eed7628f5e97b089759f65b0b4
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-01-27 10:16:00 +0100

    Add files via upload

---
+++ Sprint 1 entrega 1.5/Sprint 1.5 nivell 2 exercici 1/index.js
@@ -0,0 +1,27 @@
+//Sprint 1.5 nivell 2 exercici 1: Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
+
+function recursivePrint(message, count) {
+    console.log(message);
+    if (count > 0) {
+        setTimeout(() => {
+            recursivePrint(message, count - 1);
+        }, 1000);
+    }
+}
+
+recursivePrint("hola profe", 5);
+
+// Sprint 1.5 nivell 2 exercici 2: Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador utilizant Node Child Processes.
+
+const { exec } = require('child_process'); 
+
+function listDirectory() { 
+    const directory = process.cwd(); 
+    exec(`dir ${directory}`, (error, stdout, stderr) => { 
+        if (error) { console.error(`exec error: ${error}`); 
+        return; } 
+        
+    console.log(stdout); 
+}) } 
+    
+    listDirectory();
---
+++ Sprint 1 entrega 1.5/Sprint 1.5 nivell 2 exercici 1/readme.md
@@ -0,0 +1,7 @@
+Sprint 1.5 nivell 2 exercici 1: 
+Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
+
+----------------------------------------------------------------------------
+
+Sprint 1.5 nivell 2 exercici 2: 
+Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador utilizant Node Child Processes.
