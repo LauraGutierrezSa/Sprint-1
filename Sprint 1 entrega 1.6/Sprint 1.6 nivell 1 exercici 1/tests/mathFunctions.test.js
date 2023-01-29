b180124eecceeab0158063170f5435d2a83d779e
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-01-29 22:29:24 +0100

    Update mathFunctions.test.js

--- Sprint 1 entrega 1.6/Sprint 1.6 nivell 1 exercici 1/tests/mathFunctions.test.js
+++ Sprint 1 entrega 1.6/Sprint 1.6 nivell 1 exercici 1/tests/mathFunctions.test.js
@@ -3,32 +3,53 @@ const {sum, substract, multiply, divide  }= require('../app/mathFunctions.js');
 console.log(sum(""));
 console.log(sum("3", "4"));
 
-test('sum 3 and 4 returns 7', function () {
+describe("sum() function tests", () => {
+    test("sum of 2 and 3 should equal 5", () => {
+        expect(sum(2, 3)).toBe(5);
+    });
+    test("sum of -2 and 3 should equal 1", () => {
+        expect(sum(-2, 3)).toBe(1);
+    });
+    test("sum of 0 and 0 should equal 0", () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+});
 
-    const expectedResult = 7
+describe("substract() function tests", () => {
+    test("subtraction of 2 and 3 should equal -1", () => {
+        expect(substract(2, 3)).toBe(-1);
+    });
+    test("subtraction of -2 and 3 should equal -5", () => {
+        expect(substract(-2, 3)).toBe(-5);
+    });
+    test("subtraction of 0 and 0 should equal 0", () => {
+        expect(substract(0, 0)).toBe(0);
+    });
+});
 
-    const resultSum = sum(3, 4)
+describe("multiply() function tests", () => {
+    test("multiplication of 2 and 3 should equal 6", () => {
+        expect(multiply(2, 3)).toBe(6);
+    });
+    test("multiplication of -2 and 3 should equal -6", () => {
+        expect(multiply(-2, 3)).toBe(-6);
+    });
+    test("multiplication of 0 and 5 should equal 0", () => {
+        expect(multiply(0, 5)).toBe(0);
+    });
+});
 
-    expect( resultSum ).toBe( expectedResult )
+describe("divide() function tests", () => {
+    test("division of 6 and 3 should equal 2", () => {
+        expect(divide(6, 3)).toBe(2);
+    });
+    test("division of -6 and 3 should equal -2", () => {
+        expect(divide(-6, 3)).toBe(-2);
+    });
+    test("division of 0 and 5 should equal 0", () => {
+        expect(divide(0, 5)).toBe(0);
+    });
+    test("division of 5 and 0 should throw an error", () => {
+        expect(() => { divide(5, 0) }).toThrow();
+    });
 })
-
-
-test('add 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('Substract 3 - 2 and return 1', () => {
-    expect(substract(3, 2)).toBe(1);
-});
-
-test('multiply 2 * 3 and return 6', () => {
-    expect(multiply(2, 3)).toBe(6);
-});
-
-test('Divide 6 / 2 and return 3', () => {
-    expect(divide('6', '2')).toBe(3);
-});
-
-test('In divide function, user enters a 0 as divisor', () => {
-    expect(divide('2', 0)).toBe('Please enter a valid divisor.');
-});
