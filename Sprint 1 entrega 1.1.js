67d277c0e93e8283b9e5850f46709e34655de679
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-02-02 10:41:25 +0100

    Update Sprint 1 entrega 1.1.js

--- Sprint 1 entrega 1.1.js
+++ Sprint 1 entrega 1.1.js
@@ -27,8 +27,8 @@ const countToNine = () => {
     for (let i =0; i < 10; i++) {console.log(i)}
 }
 
-let functionArray = new Array(10)
-//alimentando el array con las funciones que cuentan de 0 a 9.
+let functionArray = new Array(10);
+
 for (let i = 0; i < functionArray.length; i++) { 
     functionArray[i] = countToNine}  
 
