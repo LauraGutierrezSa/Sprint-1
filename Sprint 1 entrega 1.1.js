145d4fdfd46e2648efba366290cfa31c32890d57
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-02-02 10:40:33 +0100

    Update Sprint 1 entrega 1.1.js

--- Sprint 1 entrega 1.1.js
+++ Sprint 1 entrega 1.1.js
@@ -1,46 +1,10 @@
 //Nivell 1 exercici 1: Crea una funció que mostri per consola el nom d'usuari/ària en invocar-la passant-li el nom com a paràmetre. Nota: Utilitza l'intèrpret de node en tots els casos.
 
-/* Dani: no et fa falta el readline, el que et demana es una funcio que faci 
-console log del parametre que se li passa. són 2 linies de codi. */
-
-/*Laura: (Estava fent això quan ho ha revisat el Dani)
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function showUsername(name) {
-  console.log(name);
-}
-
-rl.question('Please enter your username: ', (name) => {
-  showUsername(name);
-  rl.close();
-});*/
-
 function showNameThroughConsole(userName) {
   console.log(userName)
 };
 
 //Nivell 2 exercici 1: Mostra per consola el nom i cognoms de l'usuari/ària mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge. Nota: Utilitza l'intèrpret de node en tots els casos.
-
-/* Dani: semblant a l'anterior, el que et demana es tan xorra que costa d'entendre. has de declarar i inicialitzar 2 variables nom i cognom i fer un console log amb literals
-podries borrar tot excepte el console log,una linia mes amunt fer let answer = "laura" i seria mes menys el que t'esta demanant.*/
-
-/*Laura: De nou, això és el que li vaig passar al Dani per revisar:
-
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-
-const rl = readline.createInterface({ input, output });
-
-rl.question('What is your full name? Please enter your first name and your last name:', (answer) => {
-  console.log(`Your full name is ${answer}`);
-
-  rl.close();
-});*/
 
 let userName = "Laura";
 let lastName = "Gutiérrez";
@@ -49,27 +13,6 @@ console.log(`Your full name is ${userName} ${lastName}`);
 
 
 //Nivell 2 exercici 2: Invoca una funció que retorni un valor des de dins d'una template literal. Nota: Utilitza l'intèrpret de node en tots els casos.
-
-/* Dani: desastre d'enunciat, te l'escric com crec que hauria d'estar en base als feedbacks que vaig rebre jo:
-Desde dins un template literal, invoca una funcio que retorni un valor.
-*/
-
-/* Laura: No ho vaig entendre bé (el català i jo no ens portem massa bé). Això és el que li vaig lliurar:
-
-const username = "Laura";
-const futureProfession = "programmer";
-
-function invokeFunction(strings, username, futureProfession) {
-  const str0 = strings[0];
-  const str1 = strings[1]; 
-  const str2 = strings[2]; 
-
-  return `${str0}${username}${str1}${futureProfession}${str2}`;
-}
-
-const output = invokeFunction`Her name is ${username} and she will be a ${futureProfession}.`;
-
-console.log(output);*/
 
 function random(){
   return 1234;
